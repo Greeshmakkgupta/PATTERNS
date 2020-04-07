ed23adc33036e9da08c364d1b3e77fa5e3442ed6
Greeshmakkgupta <52143219+Greeshmakkgupta@users.noreply.github.com>
2020-04-07 20:57:33 +0530

    Add files via upload

---
+++ right_angled_double.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Kanak {
+    public static void main(String args[])
+    {
+        int i,j,n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                    System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+        for(i=1;i<n;i++)
+        {
+            for(j=1;j<n;j++)
+            {
+                if(j<=(n-i))
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
+
+output:
+5
+*
+**
+***
+****
+*****
+****
+*** 
+**  
+*   
