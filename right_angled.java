7f184666d3666c1561cc1fb13b9599623c6844fd
Greeshmakkgupta <52143219+Greeshmakkgupta@users.noreply.github.com>
2020-04-07 19:59:25 +0530

    Add files via upload

---
+++ right_angled.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Kanak {
+    public static void main(String args[])
+    {
+        int i,j,n;
+        int k=2;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n;j++)
+            {
+                if(j<=(n-i))
+                    System.out.print(" ");
+                else
+                    System.out.print((char)k);
+            }
+            System.out.print("\n");
+        }
+    }
+}
+output:
+7
+      
+     
+    
+   
+  
+ 
+
